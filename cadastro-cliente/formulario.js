0a7820dd4d6796d5e2b0be5410d77137002d768f
BlVck213 <paulocesarrrpontes@gmail.com>
2022-03-11 11:20:33 -0300

    Começando a responsividade e o javascript

--- cadastro-cliente/formulario.js
+++ cadastro-cliente/formulario.js
@@ -1,6 +1,33 @@
-document.querySelector("form").addEventListener("submit", event => {
-    console.log("enviar o formulário")
+"use strict"
 
-    // Evita que o fomulário seja enviado(e a tela recarregada)
-    event.preventDefault()
-})
+const formulario1 = document.getElementById("1");
+const formulario2 = document.getElementById("2");
+const formulario3 = document.getElementById("3");
+
+const button1 = document.querySelector(".button1");
+const button2 = document.querySelector(".button2");
+const button3 = document.querySelector(".button3");
+
+
+function proximo(formulario1,formulario2,formulario3){
+    if(formulario1.style.display == "flex"){
+
+        formulario1.style.display = "none";
+        formulario2.style.display = "flex";
+        
+        console.log("passou no 1")
+        
+    }
+    else if (formulario2.style.display == "flex"){
+
+        formulario2.style.display = "none";
+        formulario3.style.display = "flex";
+        console.log("Passou no 2")
+    }
+
+};
+
+document.getElementById("button1")
+    .addEventListener("click", proximo);
+
+
