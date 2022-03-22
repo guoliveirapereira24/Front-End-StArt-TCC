8fd3503e316b07c2eb162fe8a5c2904839b965c2
guoliveirapereira24 <79976904+guoliveirapereira24@users.noreply.github.com>
2022-03-22 10:57:41 -0300

    Inicio da tela Home

---
+++ home/slide.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const images = [
+    { id: '5', url: './img/' },
+    { id: '6', url: './img/' },
+    { id: '7', url: './img/' },
+    { id: '8', url: './img/' },
+    { id: '8', url: './img/' },
+    { id: '8', url: './img/' },
+    { id: '8', url: './img/' },
+    { id: '8', url: './img/' },
+    { id: '8', url: './img/' }
+];
+
+const containerItems = document.querySelector('#container-items');
+
+const loadImages = (images, container) => {
+    images.forEach((image) => {
+        container.innerHTML += `
+            <div class='item'>
+                <img src='${image.url}'
+            </div>
+        `;
+    });
+};
+
+loadImages(images, containerItems);
+
+let items = document.querySelectorAll('.item');
+
+const previous = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+};
+
+const next = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore(lastItem, items[0]);
+    items = document.querySelectorAll('.item');
+};
+
+document.querySelector('#previous').addEventListener('click', previous);
+document.querySelector('#next').addEventListener('click', next);
