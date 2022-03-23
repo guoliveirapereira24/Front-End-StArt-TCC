cca3aa8539fa17340c4f3f2342eebc96d4b443e7
BlVck213 <paulocesarrrpontes@gmail.com>
2022-03-23 11:25:44 -0300

    Estruturação da página Home concluido

--- home/slide.js
+++ home/slide.js
@@ -1,43 +1,40 @@
-'use strict';
+"use strict"
 
 const images = [
-    { id: '5', url: './img/' },
-    { id: '6', url: './img/' },
-    { id: '7', url: './img/' },
-    { id: '8', url: './img/' },
-    { id: '8', url: './img/' },
-    { id: '8', url: './img/' },
-    { id: '8', url: './img/' },
-    { id: '8', url: './img/' },
-    { id: '8', url: './img/' }
+    { 'id': '1', 'url':'./img/img1.png' },
+    { 'id': '2', 'url':'./img/noite-estrelada.webp' },
+    { 'id': '3', 'url':'./img/img1.png' },
+    { 'id': '4', 'url':'./img/noite-estrelada.webp' },
+    { 'id': '5', 'url':'./img/img1.png' },
+    { 'id': '6', 'url':'./img/noite-estrelada.webp' },
 ];
 
 const containerItems = document.querySelector('#container-items');
 
-const loadImages = (images, container) => {
-    images.forEach((image) => {
+const loadImages = ( images, container ) => {
+    images.forEach ( image => {
         container.innerHTML += `
             <div class='item'>
                 <img src='${image.url}'
             </div>
         `;
-    });
-};
+    })
+}
 
-loadImages(images, containerItems);
+loadImages( images, containerItems );
 
 let items = document.querySelectorAll('.item');
 
 const previous = () => {
     containerItems.appendChild(items[0]);
     items = document.querySelectorAll('.item');
-};
+}
 
 const next = () => {
     const lastItem = items[items.length - 1];
-    containerItems.insertBefore(lastItem, items[0]);
+    containerItems.insertBefore( lastItem, items[0] );
     items = document.querySelectorAll('.item');
-};
+}
 
 document.querySelector('#previous').addEventListener('click', previous);
 document.querySelector('#next').addEventListener('click', next);
