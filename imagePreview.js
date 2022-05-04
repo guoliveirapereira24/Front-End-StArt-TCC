f1336dcca2a0138367308b3ebd9c5df2ef9cdd0b
NOEMI RIBEIRO VICENTE PONTES <noemi.pontes@sme.prefeitura.sp.gov.br>
2022-05-03 23:28:16 -0300

    teste de upload

---
+++ imagePreview.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const imagePreview = (idFile, idImagem) => {
+    const file = document.getElementById(idFile).files[0]
+    const preview = document.getElementById(idImagem)
+    const fileReader = new FileReader()
+
+    console.log(file)
+    console.log(preview)
+
+    if (file) {
+        fileReader.readAsDataURL(file)
+    } else {
+        preview.src = '../../img/plus2.png'
+    }
+
+    fileReader.onloadend = () => (preview.src = fileReader.result)
+}
+
+export { imagePreview }
--- perfil/perfil-artista/adicionar-obra/index.js
+++ perfil/perfil-artista/adicionar-obra/index.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const tokenArtista = localStorage.getItem('tokenArtista');
+import { imagePreview } from '../../../imagePreview.js';
 
+const tokenArtista = localStorage.getItem('tokenArtista');
 const inputNome = document.getElementById('inputNome');
 const inputPreco = document.getElementById('inputPreco');
 const inputQuantidade = document.getElementById('inputQuantidade');
@@ -11,29 +12,40 @@ const textAreaDescricao = document.getElementById('textAreaDescricao');
 var selectExclusividade = document.getElementById('selectExclusividade');
 var selectCategoria = document.getElementById('selectCategoria');
 var selectSubcategoria = document.getElementById('selectSubCategoria');
-
 const btnAddObra = document.getElementById('button_adicionar_obra');
 const msgErro = document.getElementById('msgErro');
+const inputImg1Obrigatoria = document.getElementById('inputImage1');
+const inputImg2Opcional = document.getElementById('inputImage2');
+const inputImg3Opcional = document.getElementById('inputImage3');
+const inputImg4Opcional = document.getElementById('inputImage4');
+const inputImg5Opcional = document.getElementById('inputImage5');
+const inputImg6Opcional = document.getElementById('inputImage6');
 
+const configureImagePreview = () => {
+    const inputImage1 = document.querySelector('#inputImage1')
+    const handleFileImage1 = () => imagePreview('inputImage1', 'imagePreview1')
+    inputImage1.addEventListener('change', handleFileImage1)
 
-btnAddObra.addEventListener('click', () => {
-    const nome = inputNome.value;
-    const preco = inputPreco.value;
-    const quantidade = inputQuantidade.value;
-    const desconto = inputDesconto.value;
-    const tecnica = inputTecnica.value;
-    const descricao = textAreaDescricao.value;
-    const exclusividade = selectExclusividade.value;
-    const categoria = selectCategoria.value;
-    const subCategoria = selectSubcategoria.value;
-    
-    if (nome == '' || preco == '' || quantidade == '' || desconto == '' || tecnica == '' || descricao == '' || exclusividade == '' || categoria == '' || subCategoria == '') {
-        msgErro.innerText = 'Preencha todos os Campos Obrigatórios!';
-        msgErro.style.display = 'flex';
-    } else {
-        cadastrarObra(nome, preco, quantidade, desconto, tecnica, descricao, exclusividade, categoria, subCategoria);
-    }
-})
+    const inputImage2 = document.querySelector('#inputImage2')
+    const handleFileImage2 = () => imagePreview('inputImage2', 'imagePreview2')
+    inputImage2.addEventListener('change', handleFileImage2)
+
+    const inputImage3 = document.querySelector('#inputImage3')
+    const handleFileImage3 = () => imagePreview('inputImage3', 'imagePreview3')
+    inputImage3.addEventListener('change', handleFileImage3)
+
+    const inputImage4 = document.querySelector('#inputImage4')
+    const handleFileImage4 = () => imagePreview('inputImage4', 'imagePreview4')
+    inputImage4.addEventListener('change', handleFileImage4)
+
+    const inputImage5 = document.querySelector('#inputImage5')
+    const handleFileImage5 = () => imagePreview('inputImage5', 'imagePreview5')
+    inputImage5.addEventListener('change', handleFileImage5)
+
+    const inputImage6 = document.querySelector('#inputImage6')
+    const handleFileImage6 = () => imagePreview('inputImage6', 'imagePreview6')
+    inputImage6.addEventListener('change', handleFileImage6)
+}
 
 const cadastrarObra = (
     nome, 
@@ -44,45 +56,67 @@ const cadastrarObra = (
     descricao, 
     exclusividade,
     categoria,
-    subCategoria) => {
+    subCategoria,
+    inputImgObrigatoria,
+    inputImg2Opcional,
+    inputImg3Opcional,
+    inputImg4Opcional,
+    inputImg5Opcional,
+    inputImg6Opcional) => {
 
     event.preventDefault();
+    const obra =  new FormData();
 
-    const obra = {
-        "nome": nome,
-        "preco": preco,
-        "quantidade": quantidade,
-        "desconto": desconto,
-        "tecnica": tecnica,
-        "descricao": descricao,
-        "senha": senha,
-        "exclusividade": exclusividade,
-        "idCategoria": categoria,
-		"idEspecialidade": subCategoria,
-    };
+    obra.append('nomeObra', nome);
+    obra.append('preco', preco);
+    obra.append('quantidade', quantidade);
+    obra.append('desconto', desconto);
+    obra.append('tecnica', tecnica);
+    obra.append('descricao', descricao);
+    obra.append('eExclusiva', exclusividade);
+    obra.append('idCategoria', categoria);
+    obra.append('idEspecialidade', subCategoria);
+    obra.append('imagem1obrigatoria', inputImgObrigatoria, 'imagem1obrigatoria');
+    obra.append('imagem2opcional', inputImg2Opcional, 'imagem2opcional');
+    obra.append('imagem3opcional', inputImg3Opcional, 'imagem3opcional');
+    obra.append('imagem4opcional', inputImg4Opcional, 'imagem4opcional');
+    obra.append('imagem5opcional', inputImg5Opcional, 'imagem5opcional');
+    obra.append('imagem6opcional', inputImg6Opcional, 'imagem6opcional');
+    
+    
     
     const config = {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>',
+            'Content-Length': '<calculated when request is sent>',
+            'Host': '<calculated when request is sent>',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
             'Cache-Control': 'no-cache',
-            'Authorization': 'Bearer ' + tokenArtista
+            'Authorization': `Bearer ${tokenArtista}`
         },
-        body: JSON.stringify(obra)
+        body: obra
     }
     
     fetch('http://localhost:3000/obraPronta/inserirObra', config)
         .then((res) => res.json())
         .then((data) => {
-            window.location.href = "./perfil-artista.html";
-        });
+            if (data.erro) {
+                msgErro.innerText = data.erro;
+            } 
+            console.log(data);
+        })
+        .catch((err) => console.log(err));
 } 
 
 const getEspecialidades = () => {
     const config = {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
         }
     }
     
@@ -90,7 +124,6 @@ const getEspecialidades = () => {
         .then((res) => res.json())
         .then((data) => {
             const especialidades = data.especialidades;
-            console.log(especialidades)
            
            return especialidades.forEach(especialidade => {
                 const option = document.createElement('option');
@@ -101,13 +134,12 @@ const getEspecialidades = () => {
         });
 }
 
-getEspecialidades();
-
 const getCategorias = () => {
     const config = {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
         }
     }
     
@@ -115,7 +147,6 @@ const getCategorias = () => {
         .then((res) => res.json())
         .then((data) => {
             const categorias = data.categorias;
-            console.log(categorias)
            
            return categorias.forEach(categoria => {
                 const option = document.createElement('option');
@@ -126,4 +157,33 @@ const getCategorias = () => {
         });
 }
 
+btnAddObra.addEventListener('click', () => {
+    const nome = inputNome.value;
+    const preco = inputPreco.value.toString();
+    const quantidade = inputQuantidade.value.toString();
+    const desconto = inputDesconto.value.toString();
+    const tecnica = inputTecnica.value;
+    const descricao = textAreaDescricao.value;
+    const exclusividade = selectExclusividade.value;
+    const categoria = selectCategoria.value;
+    const subCategoria = selectSubcategoria.value;
+    const inputImg1 = inputImg1Obrigatoria.files[0];
+    const inputImg2 = inputImg2Opcional.files[0];
+    const inputImg3 = inputImg3Opcional.files[0];
+    const inputImg4 = inputImg4Opcional.files[0];
+    const inputImg5 = inputImg5Opcional.files[0];
+    const inputImg6 = inputImg6Opcional.files[0];
+    
+    if (nome == '' || preco == '' || quantidade == '' || desconto == '' || tecnica == '' || descricao == '' || exclusividade == '' || categoria == '' || subCategoria == '' || inputImg1 == null || inputImg1 == undefined) {
+        msgErro.innerText = 'Preencha todos os Campos Obrigatórios!';
+        msgErro.style.display = 'flex';
+    } else {
+        cadastrarObra(nome, preco, quantidade, desconto, tecnica, descricao, 
+            exclusividade, categoria, subCategoria, inputImg1, inputImg2, 
+            inputImg3, inputImg4, inputImg5, inputImg6);
+    }
+})
+
+getEspecialidades();
 getCategorias();
+configureImagePreview()
