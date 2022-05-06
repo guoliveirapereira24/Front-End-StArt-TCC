40c1d2b25e6923f5cb58c9e85a7096d5eea25c80
ENRICO741 <>
2022-05-06 10:44:03 -0300

    Tela de pedido personalizado feita

---
+++ pedido-personalizado/modal.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const fundo_modal_escolher_artista = document.getElementById("fundo_modal_escolher_artista");
+const para_alguns_artistas = document.getElementById("para_alguns_artistas");
+
+const select_visibilidade = document.getElementById("select_visibilidade");
+
+const button_cancelar = document.getElementById("cancelar");
+const text_artistas_escolhidos = document.getElementById("artistas_escolhidos");
+
